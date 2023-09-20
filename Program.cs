683151cca7f27cde3d67399c6e528f37d0761e3a
karko13 <145532713+karko13@users.noreply.github.com>
2023-09-20 09:03:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace djeljenje
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+            a = 100;
+            b = 3;
+            x = a / b;
+            y = a % b;
+
+            Console.WriteLine(a + " djeljeno sa " + b + " je " + x + " s ostatkom  " + y);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ vjezbaUvoda02.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pocetnaVjezba02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10 , b = 7;
+            double c = 12.5, d = 16.7;
+            double  rezultat = 0;
+            rezultat = a + b + c + d % 4 ;
+            Console.WriteLine("rezultat je " + rezultat );
+            Console.ReadKey();
+        }
+    }
+}
